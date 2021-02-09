ba173e8f169497b41a8929d724e0d0846230bc5d
kmarks2013 <kmarks2013@gmail.com>
2021-02-09 13:43:31 -0500

    adds export for App component

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import SearchScreen from './src/SearchScreen'
+
+export default function App() {
+
+}
