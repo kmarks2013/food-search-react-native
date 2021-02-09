09d468412dfc2eb34b3f04b6278797006cfdae45
kmarks2013 <kmarks2013@gmail.com>
2021-02-09 14:02:05 -0500

    creates Stack with createStacknavigator

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { NavigationContainer, StackActions } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import SearchScreen from './src/SearchScreen'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
