dae30d5197d14be53d28fd93b55623bdac297598
kmarks2013 <kmarks2013@gmail.com>
2021-02-09 13:51:49 -0500

    removes closing tag and turns stack.screen to a self close

--- App.js
+++ App.js
@@ -9,9 +9,7 @@ export default function App() {
         <Stack.Screen
           component={SearchScreen}
           name="Search"
-        >
-
-        </Stack.Screen>
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
