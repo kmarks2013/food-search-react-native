f3884d957c9a5f897ee80621e4a485fbcca1af12
kmarks2013 <kmarks2013@gmail.com>
2021-02-09 14:15:20 -0500

    updates location for SearchScreen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import SearchScreen from './src/Components/SearchScreen'
+import SearchScreen from './src/Screens/SearchScreen'
 
 const Stack = createStackNavigator()
 
