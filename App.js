9bd00b1c796c21b9c8d09cc849a14dfc7cdbce15
kmarks2013 <kmarks2013@gmail.com>
2021-02-09 13:44:44 -0500

    adds Stack.Navigator wrapper

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, StackActions } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import SearchScreen from './src/SearchScreen'
 
 export default function App() {
   reutrn (
     <NavigationContainer>
+      <Stack.Navigator>
 
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
