19ac854130f129283a3f7804e7cf95bdd2234148
kmarks2013 <kmarks2013@gmail.com>
2021-02-09 13:53:25 -0500

    adds react to imports

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { NavigationContainer, StackActions } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import SearchScreen from './src/SearchScreen'
